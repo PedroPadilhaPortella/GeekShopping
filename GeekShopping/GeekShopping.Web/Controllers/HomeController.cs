30d9cec8002b13d35cab8d03e851c449e4127c2d
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2024-11-11 00:02:26 -0300

    feat: create List Orders page

--- GeekShopping/GeekShopping.Web/Controllers/HomeController.cs
+++ GeekShopping/GeekShopping.Web/Controllers/HomeController.cs
@@ -65,11 +65,6 @@ namespace GeekShopping.Web.Controllers
         return View(product);
     }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
